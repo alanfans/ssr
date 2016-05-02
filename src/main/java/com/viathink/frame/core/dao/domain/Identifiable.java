772f11eb8a932dafcc6ae8bbcd98711f60a2dc4e
hylc <hylc550655145@msn.cn>
2016-05-02 23:05:37 +0800

    Rejected commit: Default

---
+++ src/main/java/com/viathink/frame/core/dao/domain/Identifiable.java
@@ -0,0 +1,27 @@
+package com.viathink.frame.core.dao.domain;
+
+import java.io.Serializable;
+
+/**
+ * 主键标识
+ *
+ * @author LiuJunGuang
+ * @date 2014年3月3日下午5:55:34
+ */
+public interface Identifiable extends Serializable {
+    /**
+     * 获取主键
+     *
+     * @return
+     * @author LiuJunGuang
+     * @date 2014年3月3日下午5:56:13
+     */
+    public String getId();
+
+    /**
+     * 设置ID属性
+     *
+     * @param id
+     */
+    public void setId(String id);
+}
